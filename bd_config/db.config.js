ec9df311ec85eb0cc4d0e6d908e5c92f9a012d51
Mauricio Garrido <mauri101100@gmail.com>
2026-04-05 17:50:53 -0400

    first commit

---
+++ bd_config/db.config.js
@@ -0,0 +1,24 @@
+export const dbConfig = {
+        HOST: 'db.ugmgbdtzajhdgghiyfas.supabase.co', 
+        USER: 'postgres', 
+        PASSWORD: 'P8GIt5WSLLW1p6mR', 
+        DB: 'postgres', 
+        DIALECT: 'postgres',
+        PORT: 5432,
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
